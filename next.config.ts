0049a8c2366d96b91cd9bafd469a0691d74147da
rahulor <rahulor2000@gmail.com>
2025-04-02 22:52:04 +0530

    update next config file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  /* config options here */
+  webpack: (config) => {
+    return config; // Ensures Webpack is used instead of Turbopack
+  },
 };
 
 export default nextConfig;
